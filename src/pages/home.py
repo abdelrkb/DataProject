b9ffb6221558ae62f564e0b41b03078b2dfa32aa
abdelrkb <abdelnour.rekkab@gmail.com>
2025-12-15 15:35:43 +0100

    wip

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,7 +1,12 @@
 from dash import html
 from src.components.graphs.hospitalisations import hospitalisation_component
+from src.components.header import header_component
+from src.components.global_stats import stats_component
 
 layout = html.Div([
-    html.H1("Dashboard COVID-19 France", style={'textAlign': 'center'}),
+    header_component,
+    stats_component,
     hospitalisation_component
-])
+],
+className="container" 
+)
