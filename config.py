6fb1d6786eeebb61b639d36898cf38a05fe4c86c
abdelrkb <abdelnour.rekkab@gmail.com>
2025-12-16 17:55:39 +0100

    fix pb chemins

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent
 
 CONFIG = {
     "DATA_PATH": {
-        "RAW": os.path.join(BASE_DIR, "data", "raw", "covid_dataset.csv"),
-        "CLEANED": os.path.join(BASE_DIR, "data", "cleaned", "covid_clean.csv"),
+        "RAW": BASE_DIR / "data" / "raw" / "covid_dataset.csv",
+        "CLEANED": BASE_DIR / "data" / "cleaned" / "covid_clean.csv",
     },
     "APP_HOST": "127.0.0.1",
     "APP_PORT": 8050,
-    "DEBUG": True
+    "DEBUG": True,
 }
